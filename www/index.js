454fbe7658f91a2069d3071d6d6789ef2a4e114e
Axel Ilmari Eirola <aeirola@fl080.local>
2017-06-28 18:49:18 +0300

    Fix back button and splas

--- www/index.js
+++ www/index.js
@@ -1,20 +1,32 @@
 "use strict";
 
 document.addEventListener("deviceready", function() {
+    function hideSplash() {
+        navigator.splashscreen.hide();
+        ref.show();
+    }
+
+    navigator.splashscreen.show();
     // Open configured site
     var ref = cordova.InAppBrowser.open(
         'https://application.methodmakia.com/login_application',
         '_blank',
-        ['location=no','toolbar=no','zoom=no','transitionstyle=crossdissolve'].join(','),
+        ['hidden=yes', 'location=no','toolbar=no','zoom=no','transitionstyle=crossdissolve'].join(','),
     );
     // Hide splash screen when content is loaded
     ref.addEventListener('loadstop', function() {
-        navigator.splashscreen.hide();
+        hideSplash();
     });
     // But also hide after two seconds, just in case
     window.setTimeout(function() {
-        navigator.splashscreen.hide();
+        hideSplash();
     }, 10000);
+
+    // Close app when web page closes
+    ref.addEventListener('exit', function() {
+        navigator.app.exitApp();
+    });
+
     // Keep screen on while application is active
     window.plugins.insomnia.keepAwake();
 }, false);
