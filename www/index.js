9f47a53e1ec6f51d82cadad33dcf4e7b7bc1e7f0
Axel Ilmari Eirola <aeirola@fl080.local>
2017-06-28 16:20:24 +0300

    Initial logic

---
+++ www/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+document.addEventListener("deviceready", function() {
+    // Open configured site
+    var ref = cordova.InAppBrowser.open(
+        'https://application.methodmakia.com/login_application',
+        '_blank',
+        ['location=no','toolbar=no','zoom=no','transitionstyle=crossdissolve'].join(','),
+    );
+    // Hide splash screen when content is loaded
+    ref.addEventListener('loadstop', function() {
+        navigator.splashscreen.hide();
+    });
+    // But also hide after two seconds, just in case
+    window.setTimeout(function() {
+        navigator.splashscreen.hide();
+    }, 2000);
+    // Keep screen on while application is active
+    window.plugins.insomnia.keepAwake();
+}, false);
